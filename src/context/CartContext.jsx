9a5ddf06a9af6015a240b122a98e3cf00b06efc2
Tara <taraparhamrad@gmail.com>
2025-10-21 21:43:19 +0330

    Added a function to provider

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,16 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import ProductList from "../Components/ProductList";
 import OrderForm from "../Components/OrderForm";
+import Cart from "../Components/Cart";
 
 export const CartContext = createContext();
 
 export default function CartProvider() {
   const [cartItems, setCartItems] = useState([]);
-  function addToCart() {}
-  function removeFromCart() {}
+  function addToCart(product) {
+    setCartItems([...cartItems, product]);
+  }
+  function removeFromCart(productName) {
+    const updatedList = cartItems.filter((item) => item.name !== productName);
+    setCartItems(updatedList);
+  }
   return (
-    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{ cartItems, setCartItems, addToCart, removeFromCart }}
+    >
       <ProductList />
+      <Cart />
       <OrderForm />
     </CartContext.Provider>
   );
